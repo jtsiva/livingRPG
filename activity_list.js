f81592a5c8f1cea60afcccbb0473473355a1e7f0
Josh Siva <jtsiva@gmail.com>
2022-08-18 19:11:31 -0700

    started basic activity list

---
+++ activity_list.js
@@ -0,0 +1,32 @@
+activity_attr_list = {
+  badminton: {
+    str: 0,
+    dex: 1,
+    foc: 0,
+    vit: 1,
+    unit: {
+      type: "time",
+      value: "00:30:00",
+    },
+  },
+  running: {
+    str: 1,
+    dex: 0,
+    foc: 0,
+    vit: 2,
+    unit: {
+      type: "time",
+      value: "00:30:00",
+    },
+  },
+  reading: {
+    str: 0,
+    dex: 0,
+    foc: 2,
+    vit: 0,
+    unit: {
+      type: "time",
+      value: "00:30:00",
+    },
+  },
+};
