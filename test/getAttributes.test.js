ed722e8d59fbdb4b691d920e3159caa543362877
Josh Siva <jtsiva@gmail.com>
2022-09-06 18:08:20 -0700

    running test with normalized unit=1

--- test/getAttributes.test.js
+++ test/getAttributes.test.js
@@ -8,4 +8,11 @@ describe("Test getAttribute", function () {
     expect(getAttributes("running", 1).foc).to.equal(0);
     expect(getAttributes("running", 1).vit).to.equal(2);
   });
+
+  it("Get attributes for running with unit=2", function () {
+    expect(getAttributes("running", 2).str).to.equal(2);
+    expect(getAttributes("running", 2).dex).to.equal(0);
+    expect(getAttributes("running", 2).foc).to.equal(0);
+    expect(getAttributes("running", 2).vit).to.equal(4);
+  });
 });
