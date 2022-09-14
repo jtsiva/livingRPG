75c63de65188026e2bb92677ba7b9675e09d1acc
Josh Siva <jtsiva@gmail.com>
2022-09-13 19:21:24 -0700

    added test scaffolding and ability to sort activities by date of activity

--- src/activityHistory.js
+++ src/activityHistory.js
@@ -1,9 +1,17 @@
+function order(a, b) {
+  return a < b ? -1 : a > b ? 1 : 0;
+}
+
 export const activityHistory = {
-  add(log, activityName, duration, intensityModifier) {
-    return log.concat({
-      activity: activityName,
-      duration: duration,
-      intensity: intensityModifier,
-    });
+  add(log, activityName, duration, intensityModifier, date) {
+    return log
+      .concat({
+        activity: activityName,
+        duration: duration,
+        intensity: intensityModifier,
+        date: date,
+      })
+      .slice()
+      .sort(order);
   },
 };
---
+++ test/activityHistory.test.js
@@ -0,0 +1,13 @@
+import { activityHistory } from "../src/activityHistory.js";
+import { expect } from "chai";
+
+describe("Testing for activityHistory", () => {
+  let log;
+  before(() => {
+    log = [];
+  });
+
+  describe("Add items to log", () => {
+    it("");
+  });
+});
